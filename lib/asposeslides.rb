018b214954e46e6562ce52ee05dca9bdf967d9bb
Sanjeev Mishra <sanjusoftware@gmail.com>
2014-12-19 17:46:14 +0530

    added ability to configure the aspose gem via the config file

--- lib/asposeslides.rb
+++ lib/asposeslides.rb
@@ -1,14 +1,30 @@
 require 'asposeslides/version'
+require 'asposeslides/powerpoint'
+require 'logger'
 require 'rjb'
 
 module Asposeslides
+
+  class << self
+    attr_accessor :aspose_slides_config
+  end
+
   def initialize_aspose
-    dir = File.join(File.dirname(File.dirname(__FILE__)), 'jars')
+    dir = Asposeslides.aspose_slides_config['jar_dir']
+
     if File.exist?(dir)
-      jardir = File.join(File.dirname(File.dirname(__FILE__)), 'jars', '**', '*.jar')
+      jardir = File.join(dir, '**', '*.jar')
     else
-      jardir = File.join('.','jars', '**', '*.jar')
+      logger = Logger.new(STDOUT)
+      logger.level = Logger::WARN
+      logger.warn('Using the non licensed aspose slides jar. Please specify path to your licensed aspose slides jar directory in config/aspose.yml file!')
+      jardir = File.join(File.dirname(File.dirname(__FILE__)),'jars', '**', '*.jar')
     end
     Rjb::load(classpath = Dir.glob(jardir).join(':'), jvmargs=['-Djava.awt.headless=true'])
+  end
+
+
+  def self.configure_aspose_slides config
+    Asposeslides.aspose_slides_config = config
   end
 end
